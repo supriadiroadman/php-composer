443eec108d196182635d973fedc0765a09df575f
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-15 15:22:36 +0700

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ec1476a79e13264cd57902437bf58090af50ec05',
+        'name' => 'schatzy/belajar_php_composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'schatzy/belajar_php_composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ec1476a79e13264cd57902437bf58090af50ec05',
+            'dev_requirement' => false,
+        ),
+    ),
+);
